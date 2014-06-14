fe46f5f0fb0be1b4b3d7ad5cf6013f6f800f6c5d
Haniel <danhorton0@gmail.com>
2014-06-13 18:33:40 -0700

    New Player Model

--- core/src/com/haniel/Shooter/entities/Player.java
+++ core/src/com/haniel/Shooter/entities/Player.java
@@ -1,5 +1,8 @@
 package com.haniel.Shooter.entities;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
@@ -13,37 +16,58 @@ import com.haniel.Shooter.weapons.PlayerGun;
 
 public class Player extends Entity{
 	
-	Texture player_forward = new Texture(Gdx.files.internal("entities/player.png"));
+	private List<Sprite> playerSprites = new LinkedList<Sprite>();
+	
     private boolean shooting = false;;
     private int maxSpeed;
     private int keyboardSpeed;
     private GameScreen gameScreen;
 	private ParticleEffect engine1Effect = new ParticleEffect();
 	private ParticleEffect engine2Effect = new ParticleEffect();
+	private float tilt, tiltSpeed;
+	private int currentSprite;
+	private boolean movingLeft, movingRight;
 
 
 	
 	public Player(Level level) {
+		playerSprites.add(new Sprite(new Texture(Gdx.files.internal("entities/player/ship-5.png"))));
+		playerSprites.add(new Sprite(new Texture(Gdx.files.internal("entities/player/ship-4.png"))));
+		playerSprites.add(new Sprite(new Texture(Gdx.files.internal("entities/player/ship-3.png"))));
+		playerSprites.add(new Sprite(new Texture(Gdx.files.internal("entities/player/ship-2.png"))));
+		playerSprites.add(new Sprite(new Texture(Gdx.files.internal("entities/player/ship-1.png"))));
+		playerSprites.add(new Sprite(new Texture(Gdx.files.internal("entities/player/ship0.png"))));
+		playerSprites.add(new Sprite(new Texture(Gdx.files.internal("entities/player/ship1.png"))));
+		playerSprites.add(new Sprite(new Texture(Gdx.files.internal("entities/player/ship2.png"))));
+		playerSprites.add(new Sprite(new Texture(Gdx.files.internal("entities/player/ship3.png"))));
+		playerSprites.add(new Sprite(new Texture(Gdx.files.internal("entities/player/ship4.png"))));
+		playerSprites.add(new Sprite(new Texture(Gdx.files.internal("entities/player/ship5.png"))));
+		
 		this.level = level;
 		this.x = 400;
 		this.y = 20;
-		this.width = 21;
-		this.height = 15;
-		this.xOffset = 4;
-		this.yOffset = 7;
-		this.sprite = new Sprite(player_forward);
+		this.width = 40;
+		this.height = 35;
+		this.xOffset = 5;
+		this.yOffset = 5;
+		this.currentSprite = 5;
+		this.sprite = playerSprites.get(currentSprite);
 		this.rectangle = new Rectangle((float) x + xOffset, (float) y + yOffset, width, height);
 		this.speed = 30;
 		this.keyboardSpeed = 11;
 		this.maxSpeed = 40;
 		this.lastShot= 0;
 		this.health = 1;
+		this.tilt = 0;
+		this.tiltSpeed = 0.02f;
+		this.movingLeft = false;
+		this.movingRight = false;
 		this.weapon = new PlayerGun(level, true);
 		this.engine1Effect.load(Gdx.files.internal("particles/PlayerEngine.p"), Gdx.files.internal("particles/"));
-		this.engine1Effect.setPosition((int)x + 11,(int) y + 1);
+		this.engine1Effect.setPosition((int)x + 22,(int) y + 5);
 		level.particleEffects.add(engine1Effect);
 		this.engine2Effect.load(Gdx.files.internal("particles/PlayerEngine.p"), Gdx.files.internal("particles/"));
-		this.engine2Effect.setPosition((int)x + 19,(int) y + 1);
+		this.engine2Effect.setPosition((int)x + 27,(int) y + 5);
 		level.particleEffects.add(engine2Effect);
 	}
 	public void update() {
@@ -64,11 +88,13 @@ public class Player extends Entity{
 	     */
 
 		if (Gdx.input.isKeyPressed(Keys.LEFT) || Gdx.input.isKeyPressed(Keys.A)) {
-			movePlayer(-keyboardSpeed , 0);		
+			movePlayer(-keyboardSpeed , 0);
+			movingLeft = true;
 	        
 		}
 	    if (Gdx.input.isKeyPressed(Keys.RIGHT) || Gdx.input.isKeyPressed(Keys.D)) {
 	    	movePlayer(keyboardSpeed , 0);
+	    	movingRight = true;
 	    	
 	    }
 	    if (Gdx.input.isKeyPressed(Keys.UP) || Gdx.input.isKeyPressed(Keys.W)) {
@@ -82,9 +108,9 @@ public class Player extends Entity{
 		
 	    if (shooting ||  Gdx.input.isKeyPressed(Keys.ENTER)) {
 	    	if ((level.getTime() - lastShot) > weapon.getFiringRate()) {
-	    		double angle = getAngleTo(x - 1, y, x - 1, y+10);
-	    		weapon.shoot(x - 1, y - 1, angle);
-	    		weapon.shoot(x + width + xOffset, y, angle);
+	    		double angle = getAngleTo(x - 1, y, x - 1, y+30);
+	    		weapon.shoot(x - 1, y + 25, angle);
+	    		weapon.shoot(x + width + xOffset, y + 25, angle);
 	    		lastShot = level.getTime();
 	    		weapon.playSound();
 	    	}
@@ -96,10 +122,31 @@ public class Player extends Entity{
 	    if (y < 0) y = 0;
 	    if (y > gameScreen.getHeight() - (height + yOffset * 2)) y = gameScreen.getHeight() - (height + yOffset *2);
 	    rectangle.setPosition((float)x + xOffset, (float)y + yOffset);
+	    // reset engines	    
+	    engine1Effect.setPosition((int)x + 22,(int) y + 5);
+	    engine2Effect.setPosition((int)x + 27,(int) y + 5);
+	    // tilt player based on direction moved
+	    if (tilt > 0) tilt -= Gdx.graphics.getDeltaTime();
+	    else if (movingLeft){
+	    	tilt = tiltSpeed;
+	    	if (currentSprite > 0) currentSprite--;
+	    	movingLeft = false;
+	    }else if (movingRight){
+	    	tilt = tiltSpeed;
+	    	if (currentSprite < 10) currentSprite++;
+	    	movingRight = false;
+	    }else{
+	    	//if not movingleftor right reposition back to center
+	    	if (currentSprite < 5) {
+	    		tilt = tiltSpeed;
+	    		currentSprite++;	    		
+	    	} else if (currentSprite > 5) {
+	    		tilt = tiltSpeed;
+	    		currentSprite--;		    		
+	    	}
+	    }
+	    this.sprite = playerSprites.get(currentSprite);
 	    
-	    
-	    engine1Effect.setPosition((int)x + 10,(int) y + 1);
-	    engine2Effect.setPosition((int)x + 17,(int) y + 1);
 
 	}
 	
@@ -107,6 +154,8 @@ public class Player extends Entity{
 		if (Math.abs(moveX) > maxSpeed) moveX = (maxSpeed * myAbs(moveX));
 		if (Math.abs(moveY) > maxSpeed) moveY = (maxSpeed * myAbs(moveY));
 		move(speed * moveX, speed * moveY);	
+		if (moveX > 0) movingRight = true;
+		else if (moveX < 0) movingLeft = true;
 	}
 	
 	public void startShooting() {
@@ -159,10 +208,10 @@ public class Player extends Entity{
 	}
 	public void resetEngines() {
 		this.engine1Effect.load(Gdx.files.internal("particles/PlayerEngine.p"), Gdx.files.internal("particles/"));
-		this.engine1Effect.setPosition((int)x + 11,(int) y + 1);
+		this.engine1Effect.setPosition((int)x + 22,(int) y + 5);
 		level.particleEffects.add(engine1Effect);
 		this.engine2Effect.load(Gdx.files.internal("particles/PlayerEngine.p"), Gdx.files.internal("particles/"));
-		this.engine2Effect.setPosition((int)x + 19,(int) y + 1);
+		this.engine2Effect.setPosition((int)x + 27,(int) y + 5);
 		level.particleEffects.add(engine2Effect);		
 	}
 }
